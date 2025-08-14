fa3b00b9e2e9a95d826c1129462193e621f705c4
Administrator <acatnight@gmail.com>
2025-08-14 23:28:16 +0800

    更新

---
+++ src/DayTwelve/Test1.java
@@ -0,0 +1,19 @@
+package DayTwelve;
+
+public class Test1 {
+    public static void main(String[] args) {
+        one1 s1 = new one1();
+        s1.name = "波妞";
+        s1.chinese = 100;
+        s1.math = 100;
+        s1.print1();
+        s1.print2();
+        one1 s2 = new one1();
+        s2.name = "波仔";
+        s2.chinese = 100;
+        s2.math = 100;
+        s2.print1();
+        s2.print2();
+
+    }
+}
---
+++ src/DayTwelve/one1.java
@@ -0,0 +1,15 @@
+package DayTwelve;
+
+public class one1 {
+    String name;
+    double chinese;
+    double math;
+
+    public void print1(){
+        System.out.println(name + "的总分为" + (chinese + math));
+    }
+    public void print2(){
+        System.out.println(name + "的平均为" + (chinese + math) / 2);
+    }
+
+}
